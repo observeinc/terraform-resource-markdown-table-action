f37c07007ae0459451fb20b7f591fa8cad518093
Ben Drucker <ben.drucker@observeinc.com>
2022-10-26 14:28:22 -0700

    generate header column with resource names/links

--- go.mod
+++ go.mod
@@ -3,9 +3,23 @@ module github.com/observeinc/terraform-resource-markdown-table-action
 go 1.19
 
 require (
+	github.com/agext/levenshtein v1.2.2 // indirect
+	github.com/apparentlymart/go-textseg v1.0.0 // indirect
+	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/hashicorp/go-version v1.6.0 // indirect
+	github.com/hashicorp/hcl v0.0.0-20170504190234-a4b07c25de5f // indirect
+	github.com/hashicorp/hcl/v2 v2.0.0 // indirect
+	github.com/hashicorp/terraform-config-inspect v0.0.0-20221020162138-81db043ad408 // indirect
+	github.com/hashicorp/terraform-exec v0.17.3 // indirect
+	github.com/hashicorp/terraform-json v0.14.0 // indirect
 	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.0 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/sethvargo/go-envconfig v0.8.0 // indirect
 	github.com/sethvargo/go-githubactions v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.3 // indirect
+	github.com/zclconf/go-cty v1.11.0 // indirect
+	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
 
+	"github.com/hashicorp/terraform-config-inspect/tfconfig"
+	"github.com/hashicorp/terraform-exec/tfexec"
 	"github.com/olekukonko/tablewriter"
 	"github.com/sethvargo/go-githubactions"
 	"gopkg.in/yaml.v3"
@@ -15,6 +20,8 @@ type Resource struct {
 }
 
 func main() {
+	workingDirectory := githubactions.GetInput("working-directory")
+
 	resources, err := getResourceInput()
 	if err != nil {
 		githubactions.Fatalf(err.Error())
@@ -24,14 +31,58 @@ func main() {
 		githubactions.Fatalf("No resources found")
 	}
 
+	tfPath, err := exec.LookPath("terraform")
+	if err != nil {
+		githubactions.Fatalf("terraform not found in PATH")
+	}
+
+	tf, err := tfexec.NewTerraform(workingDirectory, tfPath)
+	if err != nil {
+		githubactions.Fatalf(err.Error())
+	}
+
+	schemas, err := tf.ProvidersSchema(context.TODO())
+	if err != nil {
+		githubactions.Fatalf("failed to get provider schemas: %v", err)
+	}
+
+	_ = schemas
+
+	module, diags := tfconfig.LoadModule(workingDirectory)
+	if diags.HasErrors() {
+		githubactions.Fatalf("failed to load module: %v", diags.Err())
+	}
+
 	for _, resource := range resources {
 		fmt.Printf("## %s\n", resource.Name)
 
 		table := tablewriter.NewWriter(os.Stdout)
+
 		table.SetHeader(tableHeaders(resource.Attributes))
+
 		table.SetAutoFormatHeaders(false)
 		table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
 		table.SetCenterSeparator("|")
+
+		for _, r := range module.ManagedResources {
+			if r.Type != resource.Name {
+				continue
+			}
+
+			relPath, err := filepath.Rel(workingDirectory, r.Pos.Filename)
+			if err != nil {
+				githubactions.Fatalf("failed to get relative path: %v", err)
+			}
+
+			row := []string{fmt.Sprintf("[`%s`](%s#L%d)", r.Name, relPath, r.Pos.Line)}
+			for range resource.Attributes {
+				// TODO: get attribute value
+				row = append(row, "-")
+			}
+
+			table.Append(row)
+		}
+
 		table.Render()
 		fmt.Println()
 	}
