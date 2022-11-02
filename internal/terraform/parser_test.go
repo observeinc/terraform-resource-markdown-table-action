e161019bc664d56c495326ee20accfb067e2f807
Ben Drucker <ben.drucker@observeinc.com>
2022-11-02 11:34:47 -0700

    terraform: sort resources by name

--- internal/terraform/parser_test.go
+++ internal/terraform/parser_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/hashicorp/terraform-config-inspect/tfconfig"
 	tfjson "github.com/hashicorp/terraform-json"
 	"github.com/zclconf/go-cty/cty"
 )
@@ -207,6 +208,136 @@ terraform {
 
 			if diff := cmp.Diff(got, tc.want, cmpopts.IgnoreFields(UnknownAttributeValue{}, "Expr")); diff != "" {
 				t.Errorf("unexpected attributes -want +got:\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestParser_ResourcesOfType(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name         string
+		config       string
+		resourceType string
+		want         []*tfconfig.Resource
+	}{
+		{
+			name: "one",
+			config: `
+resource "test_resource_a" "test" {}
+`,
+			resourceType: "test_resource_a",
+			want: []*tfconfig.Resource{
+				{
+					Type: "test_resource_a",
+					Name: "test",
+				},
+			},
+		},
+		{
+			name:         "empty",
+			resourceType: "test_resource_a",
+			want:         []*tfconfig.Resource{},
+		},
+		{
+			name: "ignore other types",
+			config: `
+resource "test_resource_a" "test" {}
+resource "test_resource_b" "test" {}
+`,
+			resourceType: "test_resource_a",
+			want: []*tfconfig.Resource{
+				{
+					Type: "test_resource_a",
+					Name: "test",
+				},
+			},
+		},
+		{
+			name: "sort resources by name",
+			config: `
+resource "test_resource_a" "c" {}
+resource "test_resource_a" "b" {}
+resource "test_resource_a" "a" {}
+`,
+			resourceType: "test_resource_a",
+			want: []*tfconfig.Resource{
+				{
+					Type: "test_resource_a",
+					Name: "a",
+				},
+				{
+					Type: "test_resource_a",
+					Name: "b",
+				},
+				{
+					Type: "test_resource_a",
+					Name: "c",
+				},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			parser, err := NewParser(&tfjson.ProviderSchemas{
+				Schemas: map[string]*tfjson.ProviderSchema{
+					"registry.terraform.io/test/test": {
+						ResourceSchemas: map[string]*tfjson.Schema{
+							"test_resource_a": {
+								Block: &tfjson.SchemaBlock{
+									Attributes: map[string]*tfjson.SchemaAttribute{
+										"foo": {
+											AttributeType: cty.Bool,
+										},
+									},
+								},
+							},
+							"test_resource_b": {
+								Block: &tfjson.SchemaBlock{
+									Attributes: map[string]*tfjson.SchemaAttribute{
+										"foo": {
+											AttributeType: cty.Bool,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			})
+
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			dir, err := os.MkdirTemp("", "test")
+			if err != nil {
+				t.Fatal(err)
+			}
+			t.Cleanup(func() { os.RemoveAll(dir) })
+
+			if err := os.WriteFile(filepath.Join(dir, "main.tf"), []byte(tc.config), 0644); err != nil {
+				t.Fatal(err)
+			}
+
+			if err := parser.LoadModule(dir); err != nil {
+				t.Fatal(err)
+			}
+
+			got := parser.ResourcesOfType(tc.resourceType)
+
+			opts := cmp.Options{
+				cmpopts.IgnoreFields(tfconfig.Resource{}, "Provider", "Pos", "Mode"),
+			}
+
+			if diff := cmp.Diff(got, tc.want, opts); diff != "" {
+				t.Errorf("unexpected resources -want +got:\n%s", diff)
 			}
 		})
 	}
